a75e2a527892170d089c1580c4682f268b3c8e07
pankaj <pankajkumar70792@gmail.com>
2024-02-06 21:36:00 +0530

    marquee css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -206,25 +206,25 @@ const Home = () => {
             <div className="col-12">
               <div className="marquee-inner-wrapper card-wrapper">
                 <div className="d-flex">
-                  <div className="mx-4 w-25">
+                  <div className="mx-3 w-25">
                     <img src="images/brand-01.png" alt="brand" />
                   </div>
-                  <div className="mx-4 w-25">
+                  <div className="mx-3 w-25">
                     <img src="images/brand-02.png" alt="brand" />
                   </div>
-                  <div className="mx-4 w-25">
+                  <div className="mx-3 w-25">
                     <img src="images/brand-03.png" alt="brand" />
                   </div>
-                  <div className="mx-4 w-25">
+                  <div className="mx-3 w-25">
                     <img src="images/brand-04.png" alt="brand" />
                   </div>
-                  <div className="mx-4 w-25">
+                  <div className="mx-3 w-25">
                     <img src="images/brand-05.png" alt="brand" />
                   </div>
-                  <div className="mx-4 w-25">
+                  <div className="mx-3 w-25">
                     <img src="images/brand-06.png" alt="brand" />
                   </div>
-                  <div className="mx-4 w-25">
+                  <div className="mx-3 w-25">
                     <img src="images/brand-07.png" alt="brand" />
                   </div>
                 </div>
