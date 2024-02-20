2b90ba3d4379989ef0a4ea5ece808cfcda21493d
pankaj <pankajkumar70792@gmail.com>
2024-02-20 17:59:46 +0530

    minor update

--- src/componenets/Header.js
+++ src/componenets/Header.js
@@ -27,7 +27,9 @@ const Header = () => {
           <div className="row align-items-center">
             <div className="col-2">
               <h2>
-                <Link className="text-white">E-digit</Link>
+                <Link to="/" className="text-white">
+                  E-digit
+                </Link>
               </h2>
             </div>
             <div className="col-5">
