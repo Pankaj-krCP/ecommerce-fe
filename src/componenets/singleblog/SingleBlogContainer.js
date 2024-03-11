c668f6e9b46e7cfcfdddfe3e0aacd9cb25cbb397
pankaj <pankajkumar70792@gmail.com>
2024-03-11 15:00:17 +0530

    Added Dnamic Data ro BreadCrumb and Meta of single blog

--- src/componenets/singleblog/SingleBlogContainer.js
+++ src/componenets/singleblog/SingleBlogContainer.js
@@ -1,22 +1,11 @@
-import React, { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import { HiOutlineArrowLeft } from "react-icons/hi";
 import Container from "../common/Container";
-import { getABlog } from "../../features/blogs/blogSlice";
 
-const SingleBlogContainer = () => {
-  const blogState = useSelector((state) => state?.blog?.singleBlog);
-  const location = useLocation();
+const SingleBlogContainer = (props) => {
+  const { blog } = props;
   const navigate = useNavigate();
-  const getBlogId = location.pathname.split("/")[2];
-  const dispatch = useDispatch();
-  useEffect(() => {
-    getBlog();
-  }, []);
-  const getBlog = () => {
-    dispatch(getABlog(getBlogId));
-  };
   const goBack = () => {
     navigate(-1);
   };
@@ -31,17 +20,17 @@ const SingleBlogContainer = () => {
             >
               <HiOutlineArrowLeft className="fs-4" /> Go Back
             </button>
-            <h3 className="title">{blogState?.title}</h3>
+            <h3 className="title">{blog?.title}</h3>
             <img
               src={
-                blogState?.images[0]?.url
-                  ? blogState?.images[0]?.url
+                blog?.images[0]?.url
+                  ? blog?.images[0]?.url
                   : "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2021/06/Wearing_AR_Glasses-e1622692004106.jpeg"
               }
               className="img-fluid w-100 my-4"
               alt="blog"
             />
-            <p dangerouslySetInnerHTML={{ __html: blogState?.description }}></p>
+            <p dangerouslySetInnerHTML={{ __html: blog?.description }}></p>
           </div>
         </div>
       </div>
--- src/pages/SingleBlog.js
+++ src/pages/SingleBlog.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import Meta from "../componenets/common/Meta";
 import BreadCrumb from "../componenets/common/BreadCrumb";
 import SingleBlogContainer from "../componenets/singleblog/SingleBlogContainer";
+import { getABlog } from "../features/blogs/blogSlice";
 
 const SingleBlog = () => {
+  const blogState = useSelector((state) => state?.blog?.singleBlog);
+  const location = useLocation();
+  const getBlogId = location.pathname.split("/")[2];
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getABlog(getBlogId));
+  }, []);
   return (
     <>
-      <Meta title={"Dynamic Blog"} />
-      <BreadCrumb title={"Dynamic Blog"} />
-      <SingleBlogContainer />
+      <Meta title={blogState?.title} />
+      <BreadCrumb title={blogState?.title} />
+      <SingleBlogContainer blog={blogState} />
     </>
   );
 };
